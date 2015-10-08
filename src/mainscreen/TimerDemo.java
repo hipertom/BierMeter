c40dc74dc6e673536cc39a6aed377104fa2ac32d
Tom Grootjans <tom.grootjans@gmail.com>
2015-10-08 19:46:21 +0200

    Somewhat there

---
+++ src/mainscreen/TimerDemo.java
@@ -0,0 +1,26 @@
+package mainscreen;
+
+import javax.swing.Timer;
+import java.awt.event.*;
+
+public class TimerDemo implements ActionListener {
+  Timer timer = new Timer(1000,this);
+  
+  int count = 100;
+  
+  TimerDemo() {
+    timer.start();
+    }
+  
+  public static void main(int args) {
+    TimerDemo td = new TimerDemo();
+    
+    }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == timer) {
+        System.out.print(count);
+      }
+    }
+}
