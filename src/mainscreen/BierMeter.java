c0fd69404173b379a07e4b3116772969512c8e54
Tom Grootjans <tom.grootjans@gmail.com>
2015-10-12 16:02:12 +0200

    hulp van jeff

--- src/mainscreen/BierMeter.java
+++ src/mainscreen/BierMeter.java
@@ -5,109 +5,72 @@
  */
 package mainscreen;
 
-public class BierMeter extends javax.swing.JFrame {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-    public BierMeter() {
-        initComponents();
-    }
-        
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        jPanel1 = new javax.swing.JPanel();
-        PlusButton = new javax.swing.JButton();
-        TimerField = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("BierMeter");
-
-        PlusButton.setText("+ 1");
-        PlusButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                PlusButtonActionPerformed(evt);
-            }
-        });
-
-        TimerField.setText("00:00");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(192, 192, 192)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(PlusButton, javax.swing.GroupLayout.DEFAULT_SIZE, 87, Short.MAX_VALUE)
-                    .addComponent(TimerField, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(201, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(64, 64, 64)
-                .addComponent(PlusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 264, Short.MAX_VALUE)
-                .addComponent(TimerField, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(213, 213, 213))
-        );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-
-        setSize(new java.awt.Dimension(496, 679));
+/**
+ *
+ * @author Tom
+ */
+public class BierMeter extends JFrame implements ActionListener {
+    
+    boolean beginner = false;
+    int currentWaitTime = 0;
+    int beers = 0;
+    int waitTime = 60;
+    
+    private final Timer timer = new Timer(100, this);
+    private JLabel label;
+    
+    BierMeter() {
+        super("Biermeter");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(1280, 960));
+        setSize(new Dimension(1280, 960));
         setLocationRelativeTo(null);
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void PlusButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PlusButtonActionPerformed
-        // todo
-        TimerDemo TimerDemo = new TimerDemo();
-    }//GEN-LAST:event_PlusButtonActionPerformed
-
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(BierMeter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(BierMeter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(BierMeter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(BierMeter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new BierMeter().setVisible(true);
+        
+        // Zet alle panels erin    
+        init();
+        
+        // "Render" het
+        pack();
+        setVisible(true);
+    }
+    
+    private void init() {
+        JPanel container = new JPanel();
+        label = new JLabel("Je mag nu nog rijden!");
+        JButton startButton = new JButton("+1 Biertje");
+        
+        startButton.addActionListener((ActionEvent e) -> {
+            beers++;
+            currentWaitTime = waitTime * beers;
+                    
+            if(!timer.isRunning()) {
+                timer.start();
             }
         });
+        
+        container.add(startButton);
+        container.add(label);
+        add(container);
     }
-
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton PlusButton;
-    private javax.swing.JLabel TimerField;
-    private javax.swing.JPanel jPanel1;
-    // End of variables declaration//GEN-END:variables
+    
+    /*
+    * Countdown naar wanneer je weer mag rijden
+    */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(currentWaitTime == 0) {
+            label.setText("Je mag weer rijden, koekwaus.");
+        } else {
+            label.setText("Je moet " + String.valueOf(currentWaitTime--) + " seconden wachten.");
+        }
+    }
 }
--- src/mainscreen/TimerDemo.java
+++ src/mainscreen/TimerDemo.java
@@ -2,25 +2,23 @@ package mainscreen;
 
 import javax.swing.Timer;
 import java.awt.event.*;
+import javax.swing.JLabel;
 
 public class TimerDemo implements ActionListener {
-  Timer timer = new Timer(1000,this);
-  
-  int count = 100;
-  
-  TimerDemo() {
-    timer.start();
-    }
-  
-  public static void main(int args) {
-    TimerDemo td = new TimerDemo();
+    private Timer timer = new Timer(1000,this);
+    private int count = 0;
+    private JLabel label;
+ 
+    TimerDemo(JLabel countdown) {
+        this.label = countdown;
+        timer.start();
     }
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == timer) {
-        System.out.println(count);
-        count--;
-      }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+                
+        if (e.getSource() == timer) {
+            label.setText("Je moet " + String.valueOf(count--) + " seconden wachten.");
+        }
     }
 }
