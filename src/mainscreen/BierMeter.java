634e0f441fc073314616629d9ae1fbd642dd87ca
Tom Grootjans <tom.grootjans@gmail.com>
2015-10-15 11:02:10 +0200

    groot lettertype op uur min en sec

--- src/mainscreen/BierMeter.java
+++ src/mainscreen/BierMeter.java
@@ -8,6 +8,7 @@ package mainscreen;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -27,23 +28,27 @@ import javax.swing.Timer;
  * @author Tom
  */
 public class BierMeter extends JFrame implements ActionListener {
-
+    
+    // variable aanmaken
     boolean beginnerDriver = false;
     int timeToWait = 0;
     int timePassed = 0;
     int timePerDrink = 5400; // tijd wachten in sec per drankje (5400sec is 1.5uur)
     int beers = 0;
     int gramsOfAlc = 0;
-    int hours;
-    int minutes;
-    int seconds;
+    int hours = 0;
+    int minutes = 00;
+    int seconds = 00;
 
     // frames, panels etc
     private final Timer timer = new Timer(1000, this); // TIMER aanmaken, eerste getal is interval (1000 is 1 sec)
     private JButton plusBtn;
     private JLabel lblHour;
+    private JLabel lblHourTxt;
     private JLabel lblMin;
+    private JLabel lblMinTxt;
     private JLabel lblSec;
+    private JLabel lblSecTxt;
     private JPanel container;
     private JPanel timerContainer;
     
@@ -62,15 +67,32 @@ public class BierMeter extends JFrame implements ActionListener {
     }
 
     private void init() {
+        // variable definieren
         timerContainer = new JPanel();
         container = new JPanel();
-        lblHour = new JLabel("0UUR");
-        lblMin = new JLabel("0MINUTEN");
-        lblSec = new JLabel("0SECONDEN");
+        lblHour = new JLabel(Integer.toString(hours));
+        lblHourTxt = new JLabel("UUR ");
+        lblMin = new JLabel(Integer.toString(minutes));
+        lblMinTxt = new JLabel("MINUTEN ");
+        lblSec = new JLabel(Integer.toString(seconds));
+        lblSecTxt = new JLabel("SECONDEN ");
         plusBtn = new JButton("+1 Biertje");
         
+        // tekst opmaak
+        lblHour.setFont(new Font("", Font.BOLD, 52));
+        lblMin.setFont(new Font("", Font.BOLD, 52));
+        lblSec.setFont(new Font("", Font.BOLD, 52));
+        lblHour.setPreferredSize(new Dimension(58,40));
+        lblMin.setPreferredSize(new Dimension(58,40));
+        lblSec.setPreferredSize(new Dimension(58,40));
+        
+        // center layout opmaak
+        container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
+        container.setAlignmentX(Component.CENTER_ALIGNMENT);
         plusBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
         
+        
+        // action van button
         plusBtn.addActionListener((ActionEvent e) -> {
             // Uitvoeren wanneer button press
             beers++;
@@ -81,21 +103,23 @@ public class BierMeter extends JFrame implements ActionListener {
             }
         });
         
-        container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
-        container.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
     }
     
     private void render() {
-        container.add(Box.createVerticalBox());
-        
+        // inladen en weergeven van blokken in het scherm
+        //container.add(Box.createVerticalBox());
         container.add(Box.createRigidArea(new Dimension(0, 60)));
         container.add(plusBtn);
         container.add(Box.createRigidArea(new Dimension(0, 120)));
         timerContainer.add(lblHour);
+        timerContainer.add(lblHourTxt);
         timerContainer.add(lblMin);
+        timerContainer.add(lblMinTxt);
         timerContainer.add(lblSec);
+        timerContainer.add(lblSecTxt);
         container.add(timerContainer);
-        add(container, BorderLayout.CENTER);
+        this.add(container, BorderLayout.CENTER);
         
         pack();
         setVisible(true);
@@ -116,9 +140,9 @@ public class BierMeter extends JFrame implements ActionListener {
             minutes = (timeToWait % 3600) / 60;
             seconds = timeToWait % 60;
             //print uren min en sec op label
-            lblHour.setText(hours + "UUR");
-            lblMin.setText(minutes + "MINUTEN ");
-            lblSec.setText(seconds + "SECONDEN");
+            lblHour.setText(Integer.toString(hours));
+            lblMin.setText(Integer.toString(minutes));
+            lblSec.setText(Integer.toString(seconds));
             timeToWait--;
         }
     }
